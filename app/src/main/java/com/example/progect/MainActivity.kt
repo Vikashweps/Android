062485432a50476e7b238b78ef2d9b28b24ef959
Vika <ia331s30@csc.sibsutis.ru>
2025-03-13 12:56:54 +0700

    готовый калькулятор

--- app/src/main/java/com/example/progect/MainActivity.kt
+++ app/src/main/java/com/example/progect/MainActivity.kt
@@ -10,6 +10,10 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 
 import androidx.core.view.WindowInsetsCompat
+import java.lang.Exception
+
+
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,82 +25,118 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        var tvHelloWorld : TextView = findViewById(R.id.textv1)
-        tvHelloWorld.setText("Введите значения")
+        var tvHelloWorld: TextView = findViewById(R.id.textv1)
+        tvHelloWorld.setText("")
+
+        val result: TextView = findViewById(R.id.textv1) as TextView
 
         // Задаем обработчик нажатия кнопки.
-        var bButton1 : Button = findViewById<Button>(R.id.button1)
-        bButton1.setOnClickListener({
-            tvHelloWorld.setText("1")
-        });
-        var bButtonTest2 : Button = findViewById<Button>(R.id.button2)
-        bButtonTest2.setOnClickListener({
-            tvHelloWorld.setText("2")
-        });
-        var bButtonTest3 : Button = findViewById<Button>(R.id.button3)
-        bButtonTest3.setOnClickListener({
-            tvHelloWorld.setText("3")
-        });
-        var bButtonTest4 : Button = findViewById<Button>(R.id.button4)
-        bButtonTest4.setOnClickListener({
-            tvHelloWorld.setText("4")
-        });
-        var bButtonTest5 : Button = findViewById<Button>(R.id.button5)
-        bButtonTest5.setOnClickListener({
-            tvHelloWorld.setText("5")
-        });
-        var bButtonTest6 : Button = findViewById<Button>(R.id.button6)
-        bButtonTest6.setOnClickListener({
-            tvHelloWorld.setText("6")
-        });
-        var bButtonTest7 : Button = findViewById<Button>(R.id.button7)
-        bButtonTest7.setOnClickListener({
-            tvHelloWorld.setText("7")
-        });
-        var bButtonTest8 : Button = findViewById<Button>(R.id.button8)
-        bButtonTest8.setOnClickListener({
-            tvHelloWorld.setText("8")
-        });
-        var bButtonTest9 : Button = findViewById<Button>(R.id.button9)
-        bButtonTest9.setOnClickListener({
-            tvHelloWorld.setText("9")
-        });
-        var bButtonTest10 : Button = findViewById<Button>(R.id.button0)
-        bButtonTest10.setOnClickListener({
-            tvHelloWorld.setText("0")
-        });
+        var b1: Button = findViewById<Button>(R.id.button1)
+        b1.setOnClickListener({ result.append("1") });
 
-        var bButtonTest11 : Button = findViewById<Button>(R.id.buttonPlus)
-        bButtonTest11.setOnClickListener({
-            tvHelloWorld.setText("+")
-        });
-        var bButtonTest12 : Button = findViewById<Button>(R.id.buttonMinus)
-        bButtonTest12.setOnClickListener({
-            tvHelloWorld.setText("-")
-        });
-        var bButtonTest13 : Button = findViewById<Button>(R.id.buttonUmn)
-        bButtonTest13.setOnClickListener({
-            tvHelloWorld.setText("x")
-        });
-        var bButtonTest14 : Button = findViewById<Button>(R.id.buttonDel)
-        bButtonTest14.setOnClickListener({
-            tvHelloWorld.setText("/")
-        });
-        var bButtonTest15 : Button = findViewById<Button>(R.id.buttonItog)
-        bButtonTest15.setOnClickListener({
-            tvHelloWorld.setText("1")
+        var bc: Button = findViewById<Button>(R.id.buttonC)
+        bc.setOnClickListener({tvHelloWorld.setText("")});
 
-        });
-        var counter : Int = 0
-        var itog : Int
-        fun countButton(view: View) {
-            counter++
-            var tvH : TextView = findViewById(R.id.textv1)
-            tvH.setText(counter.toString())
-            var value = tvH.text.toString().toInt()
+        var bButtonTest2: Button = findViewById<Button>(R.id.button2)
+        bButtonTest2.setOnClickListener({ result.append("2") });
 
-            Log.d ("BUTTON", value.toString())
+        var bButtonTest3: Button = findViewById<Button>(R.id.button3)
+        bButtonTest3.setOnClickListener({ result.append("3") });
+
+        var bButtonTest4: Button = findViewById<Button>(R.id.button4)
+        bButtonTest4.setOnClickListener({ result.append("4") });
+
+        var bButtonTest5: Button = findViewById<Button>(R.id.button5)
+        bButtonTest5.setOnClickListener({ result.append("5") });
+
+        var bButtonTest6: Button = findViewById<Button>(R.id.button6)
+        bButtonTest6.setOnClickListener({ result.append("6") });
+
+        var bButtonTest7: Button = findViewById<Button>(R.id.button7)
+        bButtonTest7.setOnClickListener({ result.append("7") });
+
+        var bButtonTest8: Button = findViewById<Button>(R.id.button8)
+        bButtonTest8.setOnClickListener({ result.append("8") });
+
+        var bButtonTest9: Button = findViewById<Button>(R.id.button9)
+        bButtonTest9.setOnClickListener({ result.append("9") });
+
+        var bButtonTest10: Button = findViewById<Button>(R.id.button0)
+        bButtonTest10.setOnClickListener({ result.append("0") });
+
+        var bButtonTest11: Button = findViewById<Button>(R.id.buttonPlus)
+        bButtonTest11.setOnClickListener({ result.append("+") });
+
+        var bButtonTest12: Button = findViewById<Button>(R.id.buttonMinus)
+        bButtonTest12.setOnClickListener({ result.append("-") });
+
+        var bButtonTest13: Button = findViewById<Button>(R.id.buttonUmn)
+        bButtonTest13.setOnClickListener({ result.append("x") });
+
+        var bButtonTest14: Button = findViewById<Button>(R.id.buttonDel)
+        bButtonTest14.setOnClickListener({ result.append("/") });
+
+        var bButtonTest15: Button = findViewById<Button>(R.id.buttonItog)
+
+
+        fun evaluateExpression(expression: String): Double {
+            var itogo = 0.0
+
+            try {
+
+                val trimmedExpression = expression.replace(" ", "")
+                val operator = when {
+                    trimmedExpression.contains("+") -> "+"
+                    trimmedExpression.contains("-") -> "-"
+                    trimmedExpression.contains("x") -> "x"
+                    trimmedExpression.contains("/") -> "/"
+                    else -> throw IllegalArgumentException("Операция не поддерживается")
+                }
+                val numbers = trimmedExpression.split(operator)
+
+                if (numbers.size != 2) {
+                    throw IllegalArgumentException("некорректный формат")
+                }
+                val num1 = numbers[0].toDouble()
+                val num2 = numbers[1].toDouble()
+
+
+                itogo = when (operator) {
+                    "+" -> num1 + num2
+                    "-" -> num1 - num2
+                    "x" -> num1 * num2
+                    "/" -> {
+                        if (num2 == 0.0) throw ArithmeticException("нулевое значение")
+                        num1 / num2
+                    }
+
+                    else -> throw IllegalArgumentException(" ")
+                }
+
+                return itogo
+
+            } catch (e: NumberFormatException) {
+
+                throw IllegalArgumentException("Недопустимый формат чисел: " + e.message)
+
+            }
+        }
+
+        bButtonTest15.setOnClickListener {
+
+            val expression = result.text.toString()
+
+            try {
+                val result1 = evaluateExpression(expression)
+
+                result.text =String.format("%.2f",result1)
+
+            }
+            catch (e: Exception)
+            {
+                result.text = "ошибка : $e"
+            }
+
         }
     }
-
 }
